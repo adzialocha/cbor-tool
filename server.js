6dee71daf6e0e404e13819b4759c2fe9e87b4e8b
Andreas Dzialocha <x12@adz.garden>
2022-02-11 17:14:43 +0100

    Clean up

--- server.js
+++ server.js
@@ -1,47 +1,44 @@
-const path = require('path');
-const { exec } = require('child_process');
+const path = require("path");
+const { exec } = require("child_process");
 
-const cbor = require('cbor');
-const express = require('express');
-const tmp = require('tmp-file');
+const cbor = require("cbor");
+const express = require("express");
+const tmp = require("tmp-file");
 
-const TIMEOUT = 1000 * 5;
+const TIMEOUT = 1000 * 10; // in ms
 const PORT = 3000;
 
 const app = express();
-app.use(express.json());
-app.use('/', express.static(path.join(__dirname, 'static')));
+app.use("/", express.static(path.join(__dirname, "static")));
 
-app.post('/api/convert', (req, res) => {
+app.post("/api/convert", (req, res) => {
   cbor.decodeFirst(req.body.hex, (error, result) => {
     if (error) {
       res.statusMessage = `${error}`;
       res.status(400).end();
     } else {
-      res.send({ result }).end();
+      res.json({ result });
     }
   });
 });
 
-app.post('/api/validate', async(req, res) => {
+app.post("/api/validate", async (req, res) => {
   const { hex, cddl } = req.body;
 
   try {
     const cddlFile = await tmp.writeFile(cddl);
-    const cborFile = await tmp.writeFile(Buffer.from(hex, 'hex'));
+    const cborFile = await tmp.writeFile(Buffer.from(hex, "hex"));
 
-    exec(['cddl', cddlFile.path, 'validate', cborFile.path].join(' '), { timeout: TIMEOUT },
-      (error, stdout, stderr) => {
-        if (stderr) {
-          res.send({ result: stderr }).end();
-        } else if (error) {
-          res.statusMessage = `${error}`;
-          res.status(400).end();
-        } else {
-          res.send({ result: 'OK' }).end();
-        }
+    const path = `cddl ${cddlFile.path} validate ${cborFile.path}`;
+    const options = { timeout: TIMEOUT };
+
+    exec(path, options, (error, stdout, stderr) => {
+      if (stderr) {
+        res.json({ result: stderr });
+      } else {
+        res.json({ result: "OK" });
       }
-    );
+    });
   } catch (error) {
     res.statusMessage = `${error}`;
     res.status(400).end();
--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-function debounce(func, timeout = 300) {
+const debounce = (func, timeout = 300) => {
   let timer;
   return (...args) => {
     clearTimeout(timer);
@@ -6,79 +6,100 @@ function debounce(func, timeout = 300) {
       func.apply(this, args);
     }, timeout);
   };
-}
+};
 
 const store = debounce(() => {
-  const cddl = document.getElementById('input-cddl').value;
-  window.localStorage.setItem('cddl', cddl);
+  const cddl = elements["input-cddl"].value;
+  window.localStorage.setItem("cddl", cddl);
 });
 
 const load = () => {
-  const cddl = window.localStorage.getItem('cddl');
+  const cddl = window.localStorage.getItem("cddl");
   if (cddl) {
-    document.getElementById('input-cddl').value = cddl;
+    elements["input-cddl"].value = cddl;
   }
 };
 
-const request = (path, data) => {
-  return window.fetch(path.join('/'), {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json'
-    },
-  }).then((result) => {
-    if (result.status !== 200) {
-      throw Error(`${result.status} "${result.statusText}"`);
-    }
-    return result.json();
-  }).catch((error) => {
-    window.alert(`Something went wrong! ${error}`);
-    return Promise.reject(error);
-  });
+const elements = [
+  "input-cddl",
+  "input-hex",
+  "output-json",
+  "output-validation",
+  "run-convert",
+  "run-validate",
+].reduce((acc, id) => {
+  acc[id] = document.getElementById(id);
+  return acc;
+}, {});
+
+const request = async (path, data) => {
+  return window
+    .fetch(path.join("/"), {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((result) => {
+      if (result.status !== 200) {
+        throw Error(`${result.status} "${result.statusText}"`);
+      }
+      return result.json();
+    })
+    .catch((error) => {
+      window.alert(`Something went wrong! ${error}`);
+      return Promise.reject(error);
+    });
 };
 
-const convert = (hex) => {
-  document.getElementById('output-json').value = '';
-  request(['api', 'convert'], { hex }).then(({ result }) => {
-    document.getElementById('output-json').value = JSON.stringify(result, null, 2);
-  }).catch(() => {
-    // Do nothing
-  })
+const convert = async (hex) => {
+  elements["output-json"].value = "";
+
+  return request(["api", "convert"], { hex })
+    .then(({ result }) => {
+      elements["output-json"].value = JSON.stringify(result, null, 2);
+    })
+    .catch(() => {
+      // Do nothing
+    });
 };
 
-const validate = (hex, cddl) => {
-  document.getElementById('output-validation').value = '';
-  request(['api', 'validate'], { hex, cddl }).then(({ result }) => {
-    document.getElementById('output-validation').value = result;
-  }).catch(() => {
-    // Do nothing
-  })
+const validate = async (hex, cddl) => {
+  elements["output-validation"].value = "";
+
+  return request(["api", "validate"], { hex, cddl })
+    .then(({ result }) => {
+      elements["output-validation"].value = result;
+    })
+    .catch(() => {
+      // Do nothing
+    });
 };
 
-document.getElementById('run-convert').addEventListener('click', (event) => {
-  const hex = document.getElementById('input-hex').value;
+elements["run-convert"].addEventListener("click", async () => {
+  const hex = elements["input-hex"].value;
 
   if (!hex) {
-    window.alert('CBOR (HEX) input is empty.')
+    window.alert("CBOR (HEX) input is empty.");
     return;
   }
 
-  convert(hex);
+  await convert(hex);
 });
 
-document.getElementById('run-validate').addEventListener('click', (event) => {
-  const hex = document.getElementById('input-hex').value;
-  const cddl = document.getElementById('input-cddl').value;
+elements["run-validate"].addEventListener("click", async () => {
+  const hex = elements["input-hex"].value;
+  const cddl = elements["input-cddl"].value;
 
   if (!hex || !cddl) {
-    window.alert('CBOR (HEX) or CDDL input is empty.')
+    window.alert("CBOR (HEX) or CDDL input is empty.");
     return;
   }
 
-  validate(hex, cddl);
+  await validate(hex, cddl);
 });
 
-document.getElementById('input-cddl').addEventListener('input', store);
+elements["input-cddl"].addEventListener("input", store);
 
 load();
